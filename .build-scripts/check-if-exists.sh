5a1d3742c1a2bb67e9d50e06396b48e43ef4c371
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-20 07:57:09 +0800

    Delete .build-scripts directory

--- .build-scripts/check-if-exists.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -e $1 ]
-then
-    echo $1 "found"
-    exit 0
-else
-    echo $1 "not found"
-    exit 1
-fi
--- .build-scripts/check-server-dependencies.sh
+++
@@ -1,3 +0,0 @@
-path=$(grep --exclude-dir=node_modules/ --include=\*.{js,ts} -rnl $1 -e 'render(')
-
-npx dependency-check $path --missing
